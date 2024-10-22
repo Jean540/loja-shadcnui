d169b3012346b5fcc537c88f2bf1e0a9678ca0f9
Jean Araújo <jean.martins.a@gmail.com>
2024-10-22 13:39:17 -0300

    Revert "Merge branch 'bug-fixing-1.0.0'"
    This reverts commit 0741f620fb4b1ae9e1ff67d72495421ab778fad8, reversing
    changes made to 90f015feabf5a4a535fc15b8350c3fc5a4b69f40.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import { ModeToggle } from "./modeToggle";
 
 export const Header = () => {
   return (
-    <header className="flex justify-between items-center py-5 mx-3">
+    <header className="flex justify-between items-center my-5 mx-3">
       <div className="flex gap-3 items-center">
         <Logo />
         <ModeToggle />
